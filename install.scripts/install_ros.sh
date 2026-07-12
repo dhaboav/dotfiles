e281d32942befdfeba680f59358a57249789218b
dhaboav <100108392+dhaboav@users.noreply.github.com>
2026-07-12 07:21:38 +0700

    refactor: Del all file exclude the newest version of ros2

--- install.scripts/install_ros.sh
+++ install.scripts/install_ros.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
-# LAST UPDATE: SUNDAY, 22 MARCH 2026 06:13 A.M. (UTC)
-# ROS2: https://docs.ros.org/en/jazzy/Installation/Ubuntu-Install-Debs.html
+# LAST UPDATE: SUNDAY, 12 JULY 2026 12:21 A.M. (UTC)
+# ROS2: https://docs.ros.org/en/lyrical/Installation/Ubuntu-Install-Debs.html
 # GZ: https://gazebosim.org/docs/latest/ros_installation/
 
 set -e
 
-source $HOME/dotfiles/bashrc.d/commons.sh
-
 # --- ROS2 Version ---
-ROS_VERSION="jazzy"
-WS_PATH="Desktop/ros2_ws"
+ROS_VERSION="lyrical"
+WS_PATH="$HOME/Desktop/ros2_ws"
+TARGET_SHELL_RC="$HOME/.bashrc"
 
 # --- Steps ---
 # Step 1 (Configuring locale)
-printf "%s\n%s" "$(make_label "[Step 1/5] Configuring Locales..." "$color_primary")" "$color_reset"
+echo "[Step 1/6] Configuring Locales..."
 sudo apt update && sudo apt install locales software-properties-common -y
 sudo locale-gen en_US en_US.UTF-8
 sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
 
 # Step 2 (Adding ROS2 repo to our system)
-printf "%s\n%s" "$(make_label "[Step 2/5] Adding ROS2 Repository..." "$color_primary")" "$color_reset"
+echo "[Step 2/6] Adding ROS2 Repository..."
 sudo add-apt-repository universe -y
 sudo apt update && sudo apt install curl -y
 export ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrastructure/ros-apt-source/releases/latest | grep -F "tag_name" | awk -F'"' '{print $4}')
@@ -28,22 +27,37 @@ curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-a
 sudo dpkg -i /tmp/ros2-apt-source.deb
 
 # Step 3 (Installing ROS2 and Colcon)
-printf "%s\n%s" "$(make_label "[Step 3/5] Installing ROS2 and colcon..." "$color_primary")" "$color_reset"
+echo "[Step 3/6] Installing ROS2 and colcon..."
 sudo apt update && sudo apt upgrade -y
 sudo apt install ros-${ROS_VERSION}-desktop python3-colcon-common-extensions -y
 
 # Step 4 (Installing other packages for ROS2. E.g. urdf and gazebo)
-printf "%s\n%s" "$(make_label "[Step 4/5] Installing urdf and gazebo..." "$color_primary")" "$color_reset"
+echo "[Step 4/6] Installing urdf and gazebo..."
 sudo apt update && sudo apt upgrade -y
 sudo apt install ros-${ROS_VERSION}-urdf-tutorial ros-${ROS_VERSION}-ros-gz -y
 
 # Step 5 (Creating ROS2 workspace)
-printf "%s\n%s" "$(make_label "[Step 5/5] Creating ROS2 workspace..." "$color_primary")" "$color_reset"
-mkdir ${WS_PATH}/src
-cd ${WS_PATH}
+echo "[Step 5/6] Creating ROS2 workspace..."
+mkdir -p "${WS_PATH}/src"
+cd "${WS_PATH}"
 colcon build
 
+# Step 6 (Adding ROS2 setup to shell)
+echo "[Step 6/6] Adding ROS2 setup to shell..."
+cat << EOF >> "$TARGET_SHELL_RC"
+source /opt/ros/${ROS_VERSION}/setup.bash
+source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash
+WS_SETUP="$WS_PATH/install/setup.bash"
+if [[ -f "\$WS_SETUP" ]]; then
+  source "\$WS_SETUP"
+fi
+
+alias ws='cd ~/Desktop/ros2_ws'
+alias cb='colcon build'
+alias cbps='colcon_pkg_sync'
+EOF
+
 # --- Post steps ---
-printf "%s\n%s" "$(make_label "[Post] Running Talker demo ( Ctrl+C to stop )......" "$color_primary")" "$color_reset"
+echo "[Post] Running Talker demo ( Ctrl+C to stop )......"
 source /opt/ros/${ROS_VERSION}/setup.bash
 ros2 run demo_nodes_cpp talker
